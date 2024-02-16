008a63beb87af0b14c05b1de0b6a2d562488cfd2
DJHoon24 <djhoon24@gmail.com>
2024-02-15 19:42:52 -0500

    Table component initialization

---
+++ src/app/components/NBATable.tsx
@@ -0,0 +1,47 @@
+// components/NBATable.tsx
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import Image from 'next/image';
+import Player from '../models/Player';
+
+interface NBATableProps {
+  players: Player[];
+}
+
+const NBATable: React.FC<NBATableProps> = ({ players }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Player Picture</TableCell>
+            <TableCell>Player Name</TableCell>
+            <TableCell>Prop</TableCell>
+            <TableCell>Last 5 Games</TableCell>
+            <TableCell>Last 15 Games</TableCell>
+            <TableCell>This Season</TableCell>
+            <TableCell>Team</TableCell>
+            <TableCell>Book Lines</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {players.map((player) => (
+            <TableRow key={player.id}>
+              <TableCell>
+                <Image src={player.imageUrl} alt={player.name} width={50} height={50} style={{ borderRadius: '50%' }} />
+              </TableCell>
+              <TableCell>{player.name}</TableCell>
+              <TableCell>{player.prop}</TableCell>
+              <TableCell>{player.last5Games}</TableCell>
+              <TableCell>{player.last15Games}</TableCell>
+              <TableCell>{player.thisSeason}</TableCell>
+              <TableCell>{player.team}</TableCell>
+              <TableCell>{player.bookLines}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default NBATable;
---
+++ src/app/models/Player.ts
@@ -0,0 +1,35 @@
+// models/Player.ts
+
+export default class Player {
+    id: number;
+    imageUrl: string;
+    name: string;
+    prop: string;
+    last5Games: string;
+    last15Games: string;
+    thisSeason: string;
+    team: string;
+    bookLines: string;
+
+    constructor(
+        id: number,
+        imageUrl: string,
+        name: string,
+        prop: string,
+        last5Games: string,
+        last15Games: string,
+        thisSeason: string,
+        team: string,
+        bookLines: string
+    ) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.prop = prop;
+        this.last5Games = last5Games;
+        this.last15Games = last15Games;
+        this.thisSeason = thisSeason;
+        this.team = team;
+        this.bookLines = bookLines;
+    }
+}
